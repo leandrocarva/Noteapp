103599fc408ef83078438c28614bfa7675d4dc73
logonrm <logonrm@fiap.com.br>
2018-03-03 12:21:05 -0300

    Initial commit

---
+++ app/src/main/java/br/com/carvalho/noteapp/MainActivity.kt
@@ -0,0 +1,37 @@
+package br.com.carvalho.noteapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_principal, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.getItemId()) {
+            R.id.mnSalvar -> {
+
+                return true
+            }
+            R.id.mnPesquisar -> {
+
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+}
